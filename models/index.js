4c36aafdae9c968f242863e443ec997d7ab919e6
leofer28 <leo28_fernandesferreira@hotmail.com>
2021-06-17 03:05:55 -0300

    hm

--- models/index.js
+++ models/index.js
@@ -9,11 +9,9 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+
   sequelize = new Sequelize("rlpveiculos", 'root', "", config);
-}
+
 
 fs
   .readdirSync(__dirname)
